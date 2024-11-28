ffdb3dad00aacb20d6c5bcd5f19343ae9961b99d
Yahya <yahy4work@gmail.com>
2024-11-28 13:27:56 +0000

    aside done?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-
 // Next/previous controls
 function plusSlides(n) {
 showSlides(slideIndex += n);
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
 showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
 let i;
 let slides = document.getElementsByClassName("mySlides");
@@ -20,4 +17,9 @@ for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
 }
 slides[slideIndex-1].style.display = "block";
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
