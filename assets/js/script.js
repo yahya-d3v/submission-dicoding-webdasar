099df8e3cd6f475d589e7618c0636c2d3814f2dd
Yahya <yahy4work@gmail.com>
2024-11-29 14:02:11 +0000

    ui are done probably

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,14 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-showSlides(slideIndex += n);
-}
-// Thumbnail image controls
-function currentSlide(n) {
-showSlides(slideIndex = n);
-}
-function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "block";
-}
+var myIndex = 0;
+carousel();
 
-/* Dark Mode
-    function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-} 
-    */
-
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+function carousel() {
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+}
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}    
+x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
