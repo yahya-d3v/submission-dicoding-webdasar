b1a89e0b33b79538ea667d21c8232d4fd0a1e282
Yahya <yahy4work@gmail.com>
2024-11-29 14:15:58 +0000

    minor update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-var myIndex = 0;
-carousel();
+let slideIndex = 0;
 
-function carousel() {
-var i;
-var x = document.getElementsByClassName("mySlides");
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-}
-myIndex++;
-if (myIndex > x.length) {myIndex = 1}    
-x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+    function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide-image");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "none";
+
+      setTimeout(showSlides, 2000); // Change image every 2 seconds   
+
+    }
+
+    showSlides();   
