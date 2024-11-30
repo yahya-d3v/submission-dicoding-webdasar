3615b35ceffeb13709e2a311477e2ff3f0c53777
Yahya <yahy4work@gmail.com>
2024-11-30 14:19:19 +0000

    almost done but minus dot and media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-let slideIndex = 0;
-
-    function showSlides() {
+/* Dropdown */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+    }
+    }
+}
+/* Slider */
+document.addEventListener('DOMContentLoaded', function() {
+    let slideIndex = 0;
+    showSlides();
+function showSlides() {
     let i;
-    let slides = document.getElementsByClassName("slide-image");
-
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none";  
     }
-
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-
-    slides[slideIndex - 1].style.display = "none";
-
-      setTimeout(showSlides, 2000); // Change image every 2 seconds   
-
-    }
-
-    showSlides();   
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000);
+}
+});
